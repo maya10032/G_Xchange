15f00493885212f3a26179c350fae5f2b49660a4
maya10032 <maya10032azuki@gmail.com>
2024-08-26 20:09:14 +0900

    edit:ファイル整理

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,26 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 
-class Admin extends Authenticatable
+class Admin extends Model
 {
     use HasFactory;
-
-    protected $table = 'admin_users';
-
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
