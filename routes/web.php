b54ef49462b77e5dc8bed68da1792342c64c639e
maya10032 <maya10032azuki@gmail.com>
2024-08-26 14:44:32 +0900

    edit:web.php

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', [ItemController::class, 'index']);
-Route::get('/items', [ItemController::class, 'create']);
-Route::post('/items', [ItemController::class, 'store']);
-Route::delete('/items/{item}', [ItemController::class, 'destroy']);
-Route::get('/items/{item}/edit', [ItemController::class, 'edit']);
-Route::put('/items/{item}', [ItemController::class, 'update']);
-Route::get('/items/{item}/show', [ItemController::class, 'show']);
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
