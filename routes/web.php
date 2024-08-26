d97c7588a41aac0267c83ad9cd19e9ecc9d24a17
maya10032 <maya10032azuki@gmail.com>
2024-08-26 14:10:47 +0900

    add:itemsテーブル・コントローラー・モデル作成

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [ItemController::class, 'index']);
+Route::get('/items', [ItemController::class, 'create']);
+Route::post('/items', [ItemController::class, 'store']);
+Route::delete('/items/{item}', [ItemController::class, 'destroy']);
+Route::get('/items/{item}/edit', [ItemController::class, 'edit']);
+Route::put('/items/{item}', [ItemController::class, 'update']);
+Route::get('/items/{item}/show', [ItemController::class, 'show']);
