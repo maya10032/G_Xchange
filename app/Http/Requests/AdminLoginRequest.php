b2a24454f5a77cc7e63d05c7472b3a2a34b91cd0
maya10032 <maya10032azuki@gmail.com>
2024-08-27 14:04:10 +0900

    add:管理者ログイン機能追加

--- app/Http/Requests/AdminLoginRequest.php
+++ app/Http/Requests/AdminLoginRequest.php
@@ -24,9 +24,15 @@ class AdminLoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required',
             'email' => 'required',
             'password' => 'required',
         ];
+    }
+
+    public function authenticate(): void
+    {
+        if (!Auth::guard('admin')->attempt($this->only('email', 'password'))) {
+            throw ValidationException::withMessages(['failed' => __('auth.failed')]);
+        }
     }
 }
--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,33 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+
 class Admin extends Authenticatable
 {
-    use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    use HasFactory;
 }
