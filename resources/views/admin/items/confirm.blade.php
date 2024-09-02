cf3d7783114f1078a4ee784f780f3f4616ff6932
maya10032 <maya10032azuki@gmail.com>
2024-09-02 12:08:06 +0900

    update:画像アップロード機能調査

--- resources/views/admin/items/confirm.blade.php
+++ resources/views/admin/items/confirm.blade.php
@@ -7,16 +7,18 @@
     <h2>確認画面</h2>
     <form action="{{ route('admin.items.store') }}" method="POST" enctype="multipart/form-data" novalidate>
         @csrf
-        @foreach ($filePaths as $path)
-            <input type="hidden" name="file_paths[]" value="{{ $path }}">
-        @endforeach
+        @if (isset($filePaths) && is_array($filepaths))
+            @foreach ($filePaths as $path)
+                <input type="hidden" name="file_paths[]" value="{{ $path }}">
+            @endforeach
+        @endif
         @if (session('error'))
             <div class="alert alert-danger">
                 {{ session('error') }}
             </div>
         @endif
         <table class="table">
-            <tr>
+            {{-- <tr>
                 <td>商品コード</td>
                 <td>{{ $item_data['item_code'] }}</td>
             </tr>
@@ -40,22 +42,28 @@
                 <td>通常価格</td>
                 <td>{{ number_format($regularPriceWithTax) }}円</td>
             </tr>
-            <tr>
-                <td>商品画像</td>
-                <td>
-                    @foreach ($item_data['file_paths'] as $index => $path)
-                        <img src="{{ asset('storage/' . $path) }}" alt="Image {{ $index }}" width="200"
-                            height="200">
-                        @if ($index == $item_data['thumbnail'])
-                            <span>サムネイル</span>
-                        @endif
+            <tr> --}}
+            <td>商品画像</td>
+            <td>
+                @if (isset($filePaths) && is_array($filePaths))
+                    @foreach ($filePaths as $path)
+                        <input type="hidden" name="file_paths[]" value="{{ $path }}">
                     @endforeach
-                </td>
+                @endif
+
+                @foreach ($filePaths as $index => $path)
+                    <img src="{{ asset('storage/' . $path) }}" alt="Image {{ $index }}" width="200"
+                        height="200">
+                    @if ($index == $item_data['thumbnail'])
+                        <span>サムネイル</span>
+                    @endif
+                @endforeach
+            </td>
             </tr>
-            <tr>
+            {{-- <tr>
                 <td>商品説明</td>
                 <td>{!! nl2br(e($item_data['message'])) !!}</td>
-            </tr>
+            </tr> --}}
         </table>
         <button type="submit" class="btn btn-primary">登録</button>
         <button type="submit" class="btn btn-primary" name="back" value="back">戻る</button>
