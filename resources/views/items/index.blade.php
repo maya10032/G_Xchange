bcf4c388858a684f696fd7f2ba247698bb8720dc
s.taketsu <1116.prn@gmail.com>
2024-09-03 17:34:49 +0900

    update:商品詳細画面に画像挿入

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -16,36 +16,36 @@
         <tbody>
             <h2>商品一覧ページです</h2>
             @foreach ($items as $item)
-                {{-- @foreach ($item->images as $thumbnail => $image)
+                {{-- @foreach ($item->images as $thumbnail => $image) --}}
                     <tr>
-                        <td><a href="{{ route('items.show', $item->id) }}">{{ $item->item_name }}</a></td> --}}
-                {{-- </tr> --}}
-                {{-- <tr> --}}
-                {{-- <td>{{ $item->message }}</td> --}}
-                {{-- </tr> --}}
-                {{-- <tr> --}}
-                {{-- @if ($thumbnail === 0) --}}
-                {{-- <td><img src="{{ asset('storage/images/' . $item->images->first()->img_path) }}" alt="Image"
-                                style="width: 150px; height: auto;">
-                        </td> --}}
-                {{-- @endif --}}
-                {{-- @break --}}
+                        <td><a href="{{ route('items.show', $item->id) }}">{{ $item->item_name }}</a></td>
+                    </tr>
+                    <tr>
+                        <td>{{ $item->message }}</td>
+                    </tr>
+                    <tr>
+                        @if ($item->thumbnail === 0)
+                            <td><img src="{{ asset('storage/images/' . $item->images->first()->img_path) }}" alt="Image"
+                                    style="width: 150px; height: auto;">
+                            </td>
+                        @endif
+                    {{-- @break --}}
 
-                {{-- </tr>
-                {{-- @endforeach --}}
-                @foreach ($item->images as $image)
-                {{-- {{ $item->images[0]['attributes:protected']['img_path'] }} --}}
+                </tr>
+            {{-- @endforeach --}}
+            {{-- @foreach ($item->images as $image)
+                {{ $item->images[0]['attributes:protected']['img_path'] }}
                 @php
-                        $Array[] = $image['img_path'];
-                        echo '<pre>';
-                        print_r($item->images);
-                        echo '</pre>';
-                    @endphp
-                    {{-- // @if ($item->image->count() > 0)
-                    //     <img src="{{ Storage::url('images/' . $image->img_path) }}" style="width: 150px; height: auto;">
-                    // @endif --}}
-                @endforeach
-            @endforeach
+                    $Array[] = $image['img_path'];
+                    echo '<pre>';
+                    print_r($item->images);
+                    echo '</pre>';
+                @endphp
+                @if ($item->images->count() > 0)
+                    <img src="{{ Storage::url('images/' . $image->img_path) }}" style="width: 150px; height: auto;">
+                @endif
+            @endforeach --}}
+        @endforeach
 
 
 
@@ -53,8 +53,8 @@
 
 
 
-        </tbody>
-    </table>
+    </tbody>
+</table>
 @endsection
 
 {{-- {{ Debugbar::log($items->toArray()) }} --}}
