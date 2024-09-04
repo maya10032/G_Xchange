60b60f2cd974a99559216a82aaf97a1f9cefc2fc
maya10032 <maya10032azuki@gmail.com>
2024-09-04 22:07:55 +0900

    style:商品詳細

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -106,7 +106,6 @@
                                                     document.getElementById('logout-form').submit();">
                                         {{ __('ja.Logout') }}
                                     </a>
-
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
