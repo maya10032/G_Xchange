72750122c7e1b6ab95c78150e686a8b98b1cdef4
maya10032 <maya10032azuki@gmail.com>
2024-09-04 15:43:01 +0900

    style:contact_confrim

--- resources/views/contact/confirm.blade.php
+++ resources/views/contact/confirm.blade.php
@@ -3,24 +3,60 @@
 @section('title', 'お問い合わせ内容確認画面')
 
 @section('content')
-    <form method="post" action="{{ route('contact.send') }}" class="row">
-        @csrf
-        <div class="input-group-text"><i class="fa fa-id-card-o"></i>氏名</div>
-        <input disabled class="form-control" value="{{ $input['name'] }}">
-        <div class="input-group-text"><i class="fa fa-envelope"></i>メールアドレス</div>
-        <input disabled class="form-control" value="{{ $input['email'] }}">
-        <div class="input-group-text"><i class="fa fa-indent"></i>お問い合わせ項目</div>
-        <input disabled class="form-control" value="{{ $input['radio'] }}">
-        <textarea disabled rows="10" class="form-control">{{ $input['body'] }}</textarea>
-        <div class="d-grid gap-2">
-            <button type="submit" name="back" class="btn btn-outline-secondary btn-lg">
-                <i class="fa fa-mail-reply"></i>フォームに戻る
-            </button>
+    <div class="bg-info contact4 overflow-hidden position-relative">
+        <div class="row no-gutters ">
+            <div class="container">
+                <div class="col-lg-6 contact-box mb-4 mb-md-0 container">
+                    <div class="container">
+                        <h1 class="title font-weight-light mt-2">お問い合わせ内容確認</h1>
+                        <form method="post" action="{{ route('contact.send') }}" class="row">
+                            @csrf
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="form-group row mt-2">
+                                        <div class="col-sm-3 col-form-label">お名前：</div>
+                                        <div class="col-sm-9">
+                                            <input disabled class="form-control" value="{{ $input['name'] }}">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form-group row mt-2">
+                                        <div class="col-sm-3 col-form-label">メールアドレス：</div>
+                                        <div class="col-sm-9">
+                                            <input disabled class="form-control" value="{{ $input['email'] }}">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form-group row mt-2">
+                                        <div class="col-sm-3 col-form-label">お問い合わせ項目：</div>
+                                        <div class="col-sm-9">
+                                            <input disabled class="form-control" value="{{ $input['radio'] }}">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form-group row mt-2">
+                                        <div class="col-sm-3 col-form-label">メッセージ：</div>
+                                        <div class="col-sm-9">
+                                            <textarea disabled rows="10" class="form-control">{{ $input['body'] }}</textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 d-flex align-items-center mt-3">
+                                    <button type="submit" name="back" class="btn btn-secondary text-light  px-3 py-2">
+                                        フォームに戻る
+                                    </button>
+                                    <button type="submit" class="btn bg-dark text-light  px-3 py-2" style="margin-left: 10px;">
+                                        送信する
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="d-grid gap-2">
-            <button type="submit" class="btn btn-secondary btn-lg">
-                <i class="fa fa-chevron-right"></i>送信する
-            </button>
-        </div>
-    </form>
+    </div>
 @endsection
--- resources/views/contact/form.blade.php
+++ resources/views/contact/form.blade.php
@@ -18,7 +18,8 @@
                                         <div class="col-sm-9">
                                             <input id="name"
                                                 class="form-control text-black @error('name') is-invalid @enderror"
-                                                type="text" name="name" placeholder="お名前を入力してください">
+                                                type="text" name="name" placeholder="お名前を入力してください"
+                                                value="{{ old('name') }}" required autocomplete="name" autofocus>
                                             @error('name')
                                                 <div class="invalid-feedback">{{ $message }}</div>
                                             @enderror
@@ -31,7 +32,8 @@
                                         <div class="col-sm-9">
                                             <input id="email"
                                                 class="form-control text-black @error('email') is-invalid @enderror"
-                                                type="email" name="email" placeholder="メールアドレスを入力してください">
+                                                type="text" name="email" placeholder="メールアドレスを入力してください"
+                                                value="{{ old('email') }}" required autocomplete="email" autofocus>
                                             @error('email')
                                                 <div class="invalid-feedback">{{ $message }}</div>
                                             @enderror
@@ -43,24 +45,23 @@
                                         <label for="radio" class="col-sm-3 col-form-label ">お問い合わせ<br>項目：</label>
                                         <div class="col-sm-9 d-flex flex-wrap radio-group">
                                             <div class="form-check mr-3">
-                                                <input class="form-check-input" type="radio" id="item" name="radio"
-                                                    value="商品について"
+                                                <input class="form-check-input" type="radio" name="radio" value="商品について"
                                                     {{ old('radio', '商品について') === '商品について' ? 'checked' : '' }}>
                                                 <label class="form-check-label " for="item">商品について</label>
                                             </div>
                                             <div class="form-check mr-3">
-                                                <input class="form-check-input" type="radio" id="delivery" name="radio"
-                                                    value="配送について" {{ old('radio') === '配送について' ? 'checked' : '' }}>
+                                                <input class="form-check-input" type="radio" name="radio" value="配送について"
+                                                    {{ old('radio') === '配送について' ? 'checked' : '' }}>
                                                 <label class="form-check-label " for="delivery">配送について</label>
                                             </div>
                                             <div class="form-check mr-3">
-                                                <input class="form-check-input" type="radio" id="payment" name="radio"
+                                                <input class="form-check-input" type="radio" name="radio"
                                                     value="お支払いについて" {{ old('radio') === 'お支払いについて' ? 'checked' : '' }}>
                                                 <label class="form-check-label " for="payment">お支払いについて</label>
                                             </div>
                                             <div class="form-check mr-3">
-                                                <input class="form-check-input" type="radio" id="others" name="radio"
-                                                    value="その他" {{ old('radio') === 'その他' ? 'checked' : '' }}>
+                                                <input class="form-check-input" type="radio" name="radio" value="その他"
+                                                    {{ old('radio') === 'その他' ? 'checked' : '' }}>
                                                 <label class="form-check-label " for="others">その他</label>
                                             </div>
                                         </div>
@@ -71,7 +72,7 @@
                                         <label for="body" class="col-sm-3 col-form-label ">メッセージ：</label>
                                         <div class="col-sm-9">
                                             <textarea id="body" rows="8" class="form-control text-black @error('body') is-invalid @enderror"
-                                                rows="3" name="body" placeholder="お問い合わせ本文"></textarea>
+                                                rows="3" name="body" placeholder="お問い合わせ本文">{{ old('body', isset($input->body) ? $input->body : '') }}</textarea>
                                             @error('body')
                                                 <div class="invalid-feedback">{{ $message }}</div>
                                             @enderror
@@ -89,7 +90,7 @@
             </div>
             <div class="col-lg-6 right-image p-r-0">
                 <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1619902.0054433027!2d-122.68851282163044!3d37.534535608111824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80859a6d00690021%3A0x4a501367f076adff!2sSan+Francisco%2C+CA%2C+USA!5e0!3m2!1sen!2sin!4v1507725785789"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12961.897477821978!2d139.69161119999998!3d35.68994255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188cd4cfbaff57%3A0x12385d2a418fd33d!2z44CSMTYwLTAwMjMg5p2x5Lqs6YO95paw5a6_5Yy66KW_5paw5a6_!5e0!3m2!1sja!2sjp!4v1725429289273!5m2!1sja!2sjp"
                     width="100%" height="538" frameborder="0" style="border:0" allowfullscreen data-aos="fade-left"
                     data-aos-duration="3000"></iframe>
             </div>
