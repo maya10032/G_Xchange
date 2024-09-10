148188b07a60d18f1ab930664d9cc4802a1c8e55
s.taketsu <1116.prn@gmail.com>
2024-09-10 19:16:36 +0900

    style:footer契約関連

--- resources/views/company/companyprofile.blade.php
+++ resources/views/company/companyprofile.blade.php
@@ -3,76 +3,65 @@
 @section('title', '会社概要')
 
 @section('content')
-    <div class="container" style="width: 50%;">
+    <div class="container" style="width: 60%;">
         <div class="my-5">
-            <h2 class="fw-bold title--border">会社概要</h2>
-            <table class="table table-striped">
+            <h2 class="company--title--border">会社概要</h2>
+            <table class="table table-bordered">
                 <tr>
-                    <th>会社名</th>
-                    <td>G_Xchange株式会社</td>
+                    <th class="bg-body-secondary text-center">会社名</th>
+                    <td class="bg-white">G_Xchange株式会社</td>
                 </tr>
                 <tr>
-                    <th>設立</th>
-                    <td>2018年9月</td>
+                    <th class="bg-body-secondary text-center">設立</th>
+                    <td class="bg-white">2018年9月</td>
                 </tr>
                 <tr>
-                    <th>所在地</th>
-                    <td>〒160-0023 東京都新宿区</td>
+                    <th class="bg-body-secondary text-center">所在地</th>
+                    <td class="bg-white">〒160-0023 東京都新宿区</td>
                 </tr>
                 <tr>
-                    <th>資本金</th>
-                    <td>1億円</td>
+                    <th class="bg-body-secondary text-center">資本金</th>
+                    <td class="bg-white">1億円</td>
                 </tr>
                 <tr>
-                    <th>代表者</th>
-                    <td>代表取締役 猫田 たま</td>
+                    <th class="bg-body-secondary text-center">代表者</th>
+                    <td class="bg-white">代表取締役 猫田 たま</td>
                 </tr>
                 <tr>
-                    <th>従業員数</th>
-                    <td>80名</td>
+                    <th class="bg-body-secondary text-center">従業員数</th>
+                    <td class="bg-white">80名</td>
                 </tr>
                 <tr>
-                    <th>売上高</th>
-                    <td>15億円</td>
+                    <th class="bg-body-secondary text-center">売上高</th>
+                    <td class="bg-white">15億円</td>
                 </tr>
                 <tr>
-                    <th>事業内容</th>
-                    <td>リサイクル事業に関するコンプライアンス適正化及び運営管理、<br>
+                    <th class="bg-body-secondary text-center">事業内容</th>
+                    <td class="bg-white">リサイクル事業に関するコンプライアンス適正化及び運営管理、<br>
                         環境保全事業に関する企画、マネジメント及びコンサルティング</td>
                 </tr>
                 <tr>
-                    <th>取引先</th>
-                    <td>電力会社、建設会社、地方自治体、研究機関</td>
-                </tr>
-            </table>
-        </div>
-
-        <div class="my-5">
-            <h2 class="fw-bold title--border">会社沿革</h2>
-            <table class="table table-striped">
-                <tr>
-                    <th>2018年9月</th>
-                    <td>ABEテック株式会社を設立</td>
+                    <th class="bg-body-secondary text-center">取引先</th>
+                    <td class="bg-white">電力会社、建設会社、地方自治体、研究機関</td>
                 </tr>
                 <tr>
-                    <th>2019年3月</th>
-                    <td>横浜研究所開設</td>
-                </tr>
-                <tr>
-                    <th>2020年7月</th>
-                    <td>太陽光発電システム事業部を新設</td>
-                </tr>
-                <tr>
-                    <th>2021年11月</th>
-                    <td>風力発電プロジェクトを開始</td>
-                </tr>
-                <tr>
-                    <th>2022年4月</th>
-                    <td>資本金を1億円に増資</td>
-                </tr>
-                <tr>
-                    <th>2023年8月</th>
-                    <td>名古屋支社開設</td>
+                    <th class="bg-body-secondary text-center" rowspan="6">会社沿革</th>
+                    <td class="bg-white">2018年9月　G_Xchange株式会社を設立</td>
+                    <tr>
+                        <td class="bg-white">2019年3月　G研究所開設</td>
+                    </tr>
+                    <tr>
+                        <td class="bg-white">2020年7月　Xchangeシステム事業部を新設</td>
+                    </tr>
+                    <tr>
+                        <td class="bg-white">2021年11月　子猫プロジェクトを開始</td>
+                    </tr>
+                    <tr>
+                        <td class="bg-white">2022年4月　資本金を1億円に増資</td>
+                    </tr>
+                    <tr>
+                        <td class="bg-white">2023年8月　さいたま支社開設</td>
+                    </tr>
                 </tr>
             </table>
         </div>
--- resources/views/company/recruit.blade.php
+++ resources/views/company/recruit.blade.php
@@ -6,40 +6,143 @@
     <div class="py-2 container sticky-top" style="min-height: calc(80vh - 80px);">
 
         <h2 class="fw-bold title--border">採用情報</h2>
-        <div class="row row-cols-1 row-cols-md-4 g-4">
-            <div class="col">
-                <div class="card border-0" style="width: 18rem; height: 100px;">
-                    <img src="{{ asset('images/recruit_career.jpg') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h4 class="card-title">キャリア採用</h4>
+        <div class="contents">
+            <h3 class="slide-in">Jobs</h3>
+            <div class="row row-cols-1 row-cols-md-4 g-4 d-flex">
+                <div class="col">
+                    <div class="card border-0" style="width: 18rem;">
+                        <a href="#" class="image_link">
+                            <img src="{{ asset('images/recruit_career.jpg') }}" alt="キャリア採用">
+                        </a>
+                        <div class="card-body" style="height: 50px">
+                            <p>キャリア採用</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col">
-                <div class="card border-0" style="width: 18rem; height: 100px;">
-                    <img src="{{ asset('images/recruit_newgraduates.jpg') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h4 class="card-title">新卒採用</h4>
+                <div class="col">
+                    <div class="card border-0" style="width: 18rem;">
+                        <a href="#" class="image_link">
+                            <img src="{{ asset('images/recruit_newgraduates.jpg') }}" alt="新卒採用">
+                        </a>
+                        <div class="card-body 0" style="height: 50px">
+                            <p>新卒採用</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col">
-                <div class="card border-0" style="width: 18rem; height: 100px;">
-                    <img src="{{ asset('images/recruit_global.jpg') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h4 class="card-title">グローバル</h4>
+                <div class="col">
+                    <div class="card border-0" style="width: 18rem;">
+                        <a href="#" class="image_link">
+                            <img src="{{ asset('images/recruit_global.jpg') }}" alt="グローバル">
+                        </a>
+                        <div class="card-body 0" style="height: 50px">
+                            <p>グローバル</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col">
-                <div class="card border-0" style="width: 18rem; height: 100px;">
-                    <img src="{{ asset('images/recruit_disabledpeople.jpg') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h4 class="card-title">障がい者雇用</h4>
+                <div class="col">
+                    <div class="card border-0" style="width: 18rem;">
+                        <a href="#" class="image_link">
+                            <img src="{{ asset('images/recruit_disabledpeople.jpg') }}" alt="障がい者雇用">
+                        </a>
+                        <div class="card-body 0" style="height: 50px">
+                            <p>障がい者雇用</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="contents">
+            <h3 class="slide-in">Benefits</h3>
+            <div class="d-flex">
+                <div class="box3-2 btn_animation">
+                    <span class="box-title">年次有給休暇</span>
+                    <p class="btn">年次有給休暇を入社時に10日間付与します。<br>
+                        ※取得率85.0%（2023年度6月時点）</p>
+                </div>
+                <div class="box3-2 btn_animation">
+                    <span class="box-title">リラックス休暇</span>
+                    <p class="btn">年次有給休暇とは別に、年3日間の休暇を付与します。取得日の指定はありません。</p>
+                </div>
+                <div class="box3-2 btn_animation">
+                    <span class="box-title">定期健康診断</span>
+                    <p class="btn">年1回、全社員を対象に健康診断を実施しています。基本健診に加え、婦人科検診や1日人間ドックの費用も会社が補助します（年齢によって違いあり）。</p>
+                </div>
+                <div class="box3-2 btn_animation">
+                    <span class="box-title">予防接種の費用補助</span>
+                    <p class="btn">希望する社員を対象に、インフルエンザの予防接種にかかる費用を補助します。</p>
+                </div>
+            </div>
+        </div>
+        <div class="contents">
+            <h3 class="slide-in">Member</h3>
+            <div class="row row-cols-1 row-cols-md-4 g-4 d-flex">
+                <div class="col">
+                    <div class="card border-0 m-3" style="width: 18rem;">
+                        <img src="{{ asset('images/20.jpg') }}" class="card-img-top rounded-circle" alt="...">
+                        <div class="card-body" style="height: 50px">
+                            <p class="card-text text-center">代表取締役 猫田 たま</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card border-0 m-3" style="width: 18rem;">
+                        <img src="{{ asset('images/10.jpg') }}" class="card-img-top rounded-circle" alt="...">
+                        <div class="card-body" style="height: 50px">
+                            <p class="card-text text-center">代表取締役 猫田 たま</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card border-0 m-3" style="width: 18rem;">
+                        <img src="{{ asset('images/07.jpg') }}" class="card-img-top rounded-circle" alt="...">
+                        <div class="card-body" style="height: 50px">
+                            <p class="card-text text-center">代表取締役 猫田 たま</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card border-0 m-3" style="width: 18rem;">
+                        <img src="{{ asset('images/09.jpg') }}" class="card-img-top rounded-circle" alt="...">
+                        <div class="card-body" style="height: 50px">
+                            <p class="card-text text-center">代表取締役 猫田 たま</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card border-0 m-3" style="width: 18rem;">
+                        <img src="{{ asset('images/11.jpg') }}" class="card-img-top rounded-circle" alt="...">
+                        <div class="card-body" style="height: 50px">
+                            <p class="card-text text-center">代表取締役 猫田 たま</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card border-0 m-3" style="width: 18rem;">
+                        <img src="{{ asset('images/14.jpg') }}" class="card-img-top rounded-circle" alt="...">
+                        <div class="card-body" style="height: 50px">
+                            <p class="card-text text-center">代表取締役 猫田 たま</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card border-0 m-3" style="width: 18rem;">
+                        <img src="{{ asset('images/16.jpg') }}" class="card-img-top rounded-circle" alt="...">
+                        <div class="card-body" style="height: 50px">
+                            <p class="card-text text-center">代表取締役 猫田 たま</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card border-0 m-3" style="width: 18rem;">
+                        <img src="{{ asset('images/02.jpg') }}" class="card-img-top rounded-circle" alt="...">
+                        <div class="card-body" style="height: 50px">
+                            <p class="card-text text-center">代表取締役 猫田 たま</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 
 @endsection
