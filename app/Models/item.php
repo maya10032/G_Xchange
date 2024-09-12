e0013713ae70578b4da2baf74e16cd4746e0f93c
maya10032 <maya10032azuki@gmail.com>
2024-09-13 00:07:47 +0900

    Vercel設定

--- app/Models/item.php
+++ app/Models/item.php
@@ -8,8 +8,7 @@ use Kyslik\ColumnSortable\Sortable;
 
 class Item extends Model
 {
-    use HasFactory;
-    use Sortable;
+    use HasFactory,Sortable;
 
     protected $sortable = ['id', 'item_name', 'item_code', 'is_active', 'sales_price', 'created_at', 'updated_at', 'category_name', 'discount_price'];
 
@@ -27,7 +26,7 @@ class Item extends Model
         'regular_price',
         'message',
         'is_active',
-        'humbnail',
+        'thumbnail',
         'created_at'
     ];
 
