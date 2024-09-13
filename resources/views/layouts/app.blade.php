13ceb0301082dfa492dfbd4c79e1da6e1f7ba043
maya10032 <maya10032azuki@gmail.com>
2024-09-14 04:22:02 +0900

    vercell

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,18 +17,11 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-
-    <link href="http://g-xchange-ep0qln9hb-kawais-projects-0f8de189.vercel.app/css/app.css" rel="stylesheet">
-    <link rel="stylesheet"
-        href="http://g-xchange-ep0qln9hb-kawais-projects-0f8de189.vercel.app/build/assets/app-D-sv12UV.css">
-    <script type="module" src="http://g-xchange-ep0qln9hb-kawais-projects-0f8de189.vercel.app/build/assets/app-CG1jIupM.js">
-    </script>
 
     <style>
         .navbar {
