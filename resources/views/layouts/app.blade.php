462f39c6529eb3d98812b8e7e826712b5965a3f9
maya10032 <maya10032azuki@gmail.com>
2024-09-14 03:42:57 +0900

    vercell

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
@@ -122,7 +122,8 @@
                                     <small>My Page</small>
                                 </a>
                             </li>
-                            <ul class="user-aicon me-auto header-nav-custom cactus-classical-serif-regular" style="align-items: center; padding-left: 5px;;">
+                            <ul class="user-aicon me-auto header-nav-custom cactus-classical-serif-regular"
+                                style="align-items: center; padding-left: 5px;;">
                                 <li class="nav-item dropdown">
                                     <a id="navbarDropdown" class="dropdown-toggle user-link" href="#" role="button"
                                         data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
